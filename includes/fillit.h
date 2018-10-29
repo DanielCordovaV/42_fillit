d86ba4143bd4c21fcfc861b5773f552c974413f1
Julian Diaz <jdiaz@e1z2r5p8.42.us.org>
2018-10-28 17:20:16 -0700

    header file

---
+++ includes/fillit.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdiaz <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/28 17:08:38 by jdiaz             #+#    #+#             */
+/*   Updated: 2018/10/28 17:20:01 by jdiaz            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILLIT_H
+# define FILLIT_H
+
+# include <libft.h>
+
+typedef	struct		s_piece
+{
+	char	**map;
+	int		placed;
+}					t_piece;
+
+typedef struct		s_fillit
+{
+	t_piece	*list;
+	int		count;
+	char	**result;
+	int		size;
+}					t_fillit;
+#endif
